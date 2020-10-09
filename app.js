b4ae9b38ace31a3fbd4d3908e1f949293bff4f30
tfzzh <tfzzh@qq.com>
2020-10-09 17:40:59 +0800

    文件上传 文件下载

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
 const port = 3000;
 const os = require('os');
+const path = require("path");
 const router = require('./router/index');
-const openUrl = require("./utils/openUrl");//启动后自动打开浏览器
+const openUrl = require("./public/utils/openUrl");//启动后自动打开浏览器
+const uri = `http://${os.networkInterfaces()['以太网'][1].address}:${port}`;
+// 
+app.use(express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')));
+// 
+const ejs = require("ejs");
+app.set("views",path.resolve(__dirname,"./public/views"))
+app.set("view engine","ejs");
+app.engine(".html", ejs.renderFile); //设置模板渲染
+// 
 app.use(`/`, router);
-
+app.use(cors)
 // app.use(bodyParser.urlencoded({
 //    extends: true
 // }));
+
 app.listen(port, () => {
-   let uri = `http://${os.networkInterfaces()['以太网'][1].address}:${port}`;
    // openUrl(uri);
    console.log(`listening in ${uri}`);
 })
--- router/index.js
+++ router/index.js
@@ -1,6 +1,50 @@
-const express = require('express')
-const router = express.Router()
-router.get("/",(req,res)=>{
-   res.send("123")
-})
-module.exports = router
+const express = require("express");
+const app = express();
+const router = express.Router();
+const fs = require("fs");
+const upload = require('../public/utils/uploads/fileuploads');
+router.get("/", (req, res) => {
+  //   console.log("req", req);
+  res.end();
+  return;
+  res.json({
+    code: 200,
+    data: [
+      {
+        name: "芒果全屏影视会员季卡",
+        price: "600",
+        purchaseNum: 30,
+        orderTime: "2020.08.19 10:38",
+        status: 3,
+        orderNum: "32166464564646",
+      },
+    ],
+  });
+});
+//首页渲染
+router.get("/index", (req, res) => {
+  res.render("index");
+});
+//文件下载
+router.get("/download", function (req, res, next) {
+  var name = "test.txt";
+  var path = "./downloadFiles/" + name;
+  var size = fs.statSync(path).size;
+  var f = fs.createReadStream(path);
+  res.writeHead(200, {
+    "Content-Type": "application/force-download",
+    "Content-Disposition": "attachment; filename=" + name,
+    "Content-Length": size,
+  });
+  f.pipe(res);
+});
+//文件上传
+router.post("/uploadfiles",upload.single('file'), function (req, res, next) {
+   if (req.file) {
+      res.send({code:200,msg: '文件上传成功'})
+  }else{
+     res.send({code:400,msg: '参数出错'})
+  }
+
+ });
+module.exports = router;
