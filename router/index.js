c046a75943c9118294317b7633d0f78ddcf5351c
SMALLBIRD <851488172@qq.com>
2020-12-16 15:17:13 +0800

    1

--- router/index.js
+++ router/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.redirect('/index.html');
+  return
   res.json({
     code: 200,
     data: [
